b6c80bd0fdbb25143ee1ded8cba0881541d90d14
ewn2 <Travis1776>
2022-05-18 16:53:58 -0700

    added nltk downloads

--- app.py
+++ app.py
@@ -80,4 +80,8 @@ def predict():
 
 
 if __name__ == '__main__':
+    nltk.download('stopwords', quiet=True)
+    nltk.download('punkt', quiet=True)
+    nltk.download('wordnet', quiet=True)
+    nltk.download('omw - 1.4', quiet=True)
     app.run(debug=True)
